e976e4d1cd87b085af81454b7979dc2672800cb2
sevings <binque@ya.ru>
2023-04-17 19:03:22 +0200

    get, set names

--- server.go
+++ server.go
@@ -10,24 +10,28 @@ import (
 type server struct {
 	router *gin.Engine
 	server *http.Server
+	names  map[string]map[string]string
 }
 
 func newServer(address string) *server {
 	router := gin.New()
 	router.Use(gin.Recovery())
 
+	srv := &server{
+		router: router,
+		server: &http.Server{
+			Addr:    address,
+			Handler: router,
+		},
+		names: make(map[string]map[string]string),
+	}
+
 	router.Static("/assets/", "./web/assets/")
 	router.StaticFile("/", "./web/sensors.html")
+	router.POST("/[group]/[sensor]", srv.postSensorHandler())
+	router.GET("/sensors")
 
-	srv := &http.Server{
-		Addr:    address,
-		Handler: router,
-	}
-
-	return &server{
-		router: router,
-		server: srv,
-	}
+	return srv
 }
 
 func (srv *server) SetWsHandler(handler gin.HandlerFunc) {
@@ -45,5 +49,29 @@ func (srv *server) Shutdown(ctx context.Context) {
 	err := srv.server.Shutdown(ctx)
 	if err != nil {
 		log.Fatal(err.Error())
+	}
+}
+
+func (srv *server) postSensorHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		group := ctx.Param("group")
+		sensor := ctx.Param("sensor")
+		name := ctx.PostForm("name")
+
+		groupMap, ok := srv.names[group]
+		_, ok = groupMap[sensor]
+		if !ok {
+			ctx.AbortWithStatus(400)
+			return
+		}
+
+		groupMap[sensor] = name
+		ctx.Status(200)
+	}
+}
+
+func (srv *server) getSensorsHandler() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		ctx.JSON(200, srv.names)
 	}
 }
